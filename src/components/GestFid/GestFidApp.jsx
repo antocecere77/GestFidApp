25726e0351b3e30a46501098e796994645c4a370
antocecere77 <antocecere77@gmail.com>
2020-05-20 16:10:22 +0200

    Creazione componente login

--- src/components/GestFid/GestFidApp.jsx
+++ src/components/GestFid/GestFidApp.jsx
@@ -12,13 +12,36 @@ export default class GestFidApp extends Component {
 }
 
 class LoginComponent extends Component {
+
+    state = {
+        userId: 'Antonio',
+        password: ''
+    }
+
     render() {
         return (
             <div>
-               Nome Utente: <input type="text" name="user"/>
-               Password: <input type="password" name="password"/>
-               <button>Accedi</button>
+               Nome Utente: <input type="text" name="user" value={this.state.userId} onChange={this.gestModUser} />
+               Password: <input type="password" name="password" value={this.state.password} onChange={this.gestModPassword}/>
+               <button onClick={this.login}>Accedi</button>
             </div>
         );
+    }
+
+    login = () => {
+        console.log(this.state.userId);
+        console.log(this.state.password);
+    }
+
+    gestModUser = (event) => {
+        this.setState({
+            userId: event.target.value
+        });
+    }
+
+    gestModPassword = (event) => {
+        this.setState({
+            password: event.target.value
+        });
     }
 }
