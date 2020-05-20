ed9e3fe87cc4c6f281bf121ebe84ca39e93621de
antocecere77 <antocecere77@gmail.com>
2020-05-20 15:59:59 +0200

    Creazione componente Login

--- src/components/GestFid/GestFidApp.jsx
+++ src/components/GestFid/GestFidApp.jsx
@@ -4,7 +4,20 @@ export default class GestFidApp extends Component {
     render() {
         return (
             <div className={GestFidApp}>
-                <p>Ciao Sono il componente GestFidApp</p>
+                <p>Accedi alla App GestFid</p>
+                <LoginComponent />
+            </div>
+        );
+    }
+}
+
+class LoginComponent extends Component {
+    render() {
+        return (
+            <div>
+               Nome Utente: <input type="text" name="user"/>
+               Password: <input type="password" name="password"/>
+               <button>Accedi</button>
             </div>
         );
     }
