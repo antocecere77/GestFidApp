d79affe94db126255953c6785cc9ade30ec5af1f
antocecere77 <antocecere77@gmail.com>
2020-05-20 17:27:15 +0200

    Creazione di un link alla pagina clienti

--- src/components/GestFid/Welcome.jsx
+++ src/components/GestFid/Welcome.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Welcome extends Component {
     render() {
         return (
             <div className="WelcomeComponent">
                 <p>Benvenuti in GestFid</p>
-                <p>Saluti {this.props.match.params.userid}</p>
+                <p>Saluti {this.props.match.params.userid}, clicca <Link to="/clienti">qui</Link> per vedere i clienti disponibili</p>
             </div>
         );
     }
